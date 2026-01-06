d61fc9cda98335c5ab9acbcfcc90635f7c1f01a2
shivam3821 <shivamsinharewa@gmail.com>
2026-01-06 07:42:05 +0530

    Requirements

--- x.py
+++
@@ -1,9 +0,0 @@
-import streamlit
-import numpy
-import pandas
-import sklearn
-
-print("streamlit:", streamlit.__version__)
-print("numpy:", numpy.__version__)
-print("pandas:", pandas.__version__)
-print("scikit-learn:", sklearn.__version__)
